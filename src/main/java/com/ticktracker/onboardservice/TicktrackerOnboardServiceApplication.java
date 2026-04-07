7d80390faf2dfd30d56668c943eb5378a69c87f4
Aritra Sarkar <aritrasarkaraec@gmail.com>
2026-04-07 18:31:41 +0530

    Initial Commit

---
+++ src/main/java/com/ticktracker/onboardservice/TicktrackerOnboardServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ticktracker.onboardservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicktrackerOnboardServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicktrackerOnboardServiceApplication.class, args);
+	}
+
+}
